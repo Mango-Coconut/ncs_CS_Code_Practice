d51003220690d4c55a22780d66149053ab481e35
OhSeungSe <39735663+Mango-Coconut@users.noreply.github.com>
2025-07-25 15:50:14 +0900

    250725

--- CSStudy.cs
+++ CSStudy.cs
@@ -13,7 +13,6 @@ class CSStudy
         Bottom = 4,
         Left = 8
     }
-    
     public void EnumExample()
 {
 
@@ -27,18 +26,5 @@ class CSStudy
     }
 }
 
-    public void HashTableExample()
-    {
-        int[,] ints = { { 1, 2 }, { 3, 4 } };
-    }
-    public int solution(string myString, string pat) {
-        StringBuilder str = new StringBuilder();
-        
-        foreach (char ch in myString)
-        {
-            str.Append(ch == 'A' ? 'B' : 'A');
-        }
-        
-        return str.ToString().Contains(pat) ? 1 : 0;
-    }
+    
 }
--- Program.cs
+++ Program.cs
@@ -10,9 +10,26 @@ internal class Program
         Solution sol = new Solution();
         CSStudy cs = new CSStudy();
 
-        cs.EnumExample();
 
-        
+        Console.Write($"{GetNumber()}, ");
+        Console.Write($"{GetNumber()}, ");
+        Console.Write($"{GetNumber()}, ");
+        /*
+        foreach (int i in GetNumber())
+        {
+            Console.Write($"{i}, ");
+        }
+        Console.WriteLine();
+        Console.WriteLine(GetNumber().ToArray()[0]);
+        */
     }
 
+    static IEnumerable<int> GetNumber()
+    {
+        int n = 1;
+        while (n < 10)
+        {
+            yield return n++ * 10;
+        }
+    }
 }
